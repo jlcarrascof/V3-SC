98ba4e610d4e2cc3094006c516450968d375083e
Javier Martinez <entrenamientospuntonet@gmail.com>
2026-01-08 20:49:23 -0400

    feat: add conditional rendering component with reactive properties

---
+++ intro-vue-edteam/src/components/conditional-test.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+    import { ref } from 'vue';
+
+    const text = ref<string>("Hello, EDTeam")
+    const isVisible = ref<boolean>(true)
+
+
+</script>
+
+<template>
+    <div v-if="isVisible">
+        {{ text }}
+    </div>
+</template>
+
+<style scoped>
+
+</style>
