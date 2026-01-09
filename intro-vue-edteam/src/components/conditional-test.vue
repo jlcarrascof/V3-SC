4215945f227a6dec5a07da2f20717c45f3bf93f1
Javier Martinez <entrenamientospuntonet@gmail.com>
2026-01-08 21:07:34 -0400

    feat: enhance conditional rendering with user input and age check

--- intro-vue-edteam/src/components/conditional-test.vue
+++ intro-vue-edteam/src/components/conditional-test.vue
@@ -3,11 +3,19 @@
 
     const text = ref<string>("Hello, EDTeam")
     const isVisible = ref<boolean>(true)
+    const age = ref<number>(15)
+    const description = ref<string>("")
 </script>
 
 <template>
+    <input v-model="description" />
     <div v-if="isVisible">
         {{ text }}
+    </div>
+    <div v-else-if="age > 18">
+        Hello from the second block!!
+        <br />
+        <span v-show="description.length > 0">{{ description }}</span>
     </div>
     <div v-else>
         <span>Alternative Text</span>
