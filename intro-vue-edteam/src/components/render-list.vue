1aef9a8428bc090eebec1e5f323350ebc26b1d67
Javier Martinez <entrenamientospuntonet@gmail.com>
2026-01-09 22:42:31 -0400

    feat: add user interface and initialize users data in render-list component

--- intro-vue-edteam/src/components/render-list.vue
+++ intro-vue-edteam/src/components/render-list.vue
@@ -1,13 +1,48 @@
 <script setup lang="ts">
     import { ref } from 'vue';
 
+    interface User {
+        id: number,
+        firstName: string,
+        secondName: string,
+        age: number
+    }
+
     const names = ref<string[]>([
         'John', 'Juan', 'Jose', 'Christian', 'Carlos'
     ])
+
+    const users = ref<User[]>([{
+        id: 1,
+        firstName: 'John',
+        secondName: 'Doe',
+        age: 30
+    }, {
+        id: 2,
+        firstName: 'Jane',
+        secondName: 'Smith',
+        age: 25 
+    }, {
+        id: 3,
+        firstName: 'Mike',
+        secondName: 'Johnson',
+        age: 40
+    }, {
+        id: 4,
+        firstName: 'Emily',
+        secondName: 'Davis',
+        age: 35        
+    }, {
+        id: 5,
+        firstName: 'David',
+        secondName: 'Wilson',
+        age: 28
+    }]);
 </script>
 
 <template>
     <div>
+        <h2>Names</h2>
         <ul>
             <li v-for="name in names" :key="name">
                 {{ name }}
