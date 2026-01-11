26024722113d1a93606bb1e63c37345b9cd2dd21
Javier Martinez <entrenamientospuntonet@gmail.com>
2026-01-10 20:43:52 -0400

    feat: add computed-sample component with reactive cart data

---
+++ intro-vue-edteam/src/components/computed-sample.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import { reactive } from 'vue';
+
+    const cart = reactive({
+        category: 'hosting',
+        description: 'EDTeam shopping cart',
+        currency: 'USD',
+        items: [
+            {
+                id: 1,
+                description: 'Basic',
+                price: 20,
+            },
+            {
+                id: 2,
+                description: 'Medium',
+                price: 30,
+            },            {
+                id: 3,
+                description: 'Pro',
+                price: 40,
+            },            
+        ]
+    })
+</script>
+
+<template>
+    <div>
+
+    </div>
+</template>
+
+<style scoped>
+
+</style>
