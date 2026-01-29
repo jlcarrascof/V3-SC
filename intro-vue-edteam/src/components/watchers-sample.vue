e6f5bc91c96db8376b4772a2f01683d1bf19ec14
Javier Martinez <entrenamientospuntonet@gmail.com>
2026-01-28 21:43:19 -0400

    feat: add initial watchers-sample component with setup script and template

---
+++ intro-vue-edteam/src/components/watchers-sample.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+    import { ref } from 'vue';
+
+    const message = ref<string>(`Hi, EDTeam`)
+    const error = ref<string>('')
+</script>
+
+<template>
+    <div>
+
+    </div>
+</template>
+
+<style scoped>
+
+</style>
